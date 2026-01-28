4593661db7ebfec8523f59161256d36a8902dd51
Timothy Hutchins <tgh6407@gmail.com>
2026-01-28 15:58:36 -0600

    Temp

--- system/include/ALPM/Transaction.hpp
+++ system/include/ALPM/Transaction.hpp
@@ -4,6 +4,8 @@
 #include "Database.hpp"
 
 #include <vector>
+#include <bitset>
+#include <queue>
 
 namespace ALPM {
     class Transaction {
@@ -11,33 +13,63 @@ namespace ALPM {
         public:
             enum class PackageOperation {
                 Install,
-                Uninstall,
-                Update
+                Uninstall
             };
 
             enum class DatabaseOperation {
                 Update
             };
 
+            enum class OperationFlags {
+                /* libalpm flags */
+                NoDependencyChecks = 1 << 0,
+                NoSaveBackups = 1 << 2,
+                NoDepVersionChecking = 1 << 3,
+                RemoveDependingPackages = 1 << 4,
+                Recursive = 1 << 5,
+                DatabaseOnly = 1 << 6,
+                NoHooks = 1 << 7,
+                AllDependencies = 1 << 8,
+                DownloadOnly = 1 << 9,
+                NoScriptlets = 1 << 10,
+                NoConflicts = 1 << 11,
+                AsNeeded = 1 << 13,
+                AllExplicit = 1 << 14,
+                OnlyRemoveUnneeded = 1 << 15,
+                RecursiveAll = 1 << 16,
+                NoLock = 1 << 17
+
+                /* Wrapper flags */
+            };
+
             static auto Create() -> std::shared_ptr<Transaction>;
+            static auto ApplyAll() -> void;
 
             Transaction(Private);
             ~Transaction() = default;
 
+            auto SetFlags(const std::bitset<32> &transactionFlags);
             auto AddPackageOperation(const Package &package, PackageOperation operation) -> void;
             auto AddDatabaseOperation(const Database &database, DatabaseOperation operation) -> void;
+            auto AddSystemUpgradeOperation() -> void;
 
-            auto GetPackageUpdates() const -> std::vector<Package>;
             auto GetPackageInstalls() const -> std::vector<Package>;
             auto GetPackageUninstalls() const -> std::vector<Package>;
 
             auto GetDatabaseUpdates() const -> std::vector<Database>;
 
-        private:
+            auto Apply() const -> void;
 
+        private:
+            bool m_valid{true};
+            inline static std::queue<std::shared_ptr<Transaction>> s_transactions{};
             static auto CheckPackageOperation(std::pair<Package, PackageOperation> package, PackageOperation operation) -> bool;
+
+            std::bitset<32> m_transactionFlags;
 
             std::vector<std::pair<Package, PackageOperation>> m_packageOperations;
             std::vector<std::pair<Database, DatabaseOperation>> m_databaseOperations;
+
+            bool m_systemUpgrade{false};
     };
 };
--- system/src/ALPM/Transaction.cpp
+++ system/src/ALPM/Transaction.cpp
@@ -1,6 +1,10 @@
 #include "Transaction.hpp"
+#include "ALPM.hpp"
+
+#include <alpm.h>
 
 #include <ranges>
+#include <format>
 
 using namespace ALPM;
 
@@ -18,10 +22,8 @@ auto Transaction::AddDatabaseOperation(const Database &database, DatabaseOperati
     m_databaseOperations.emplace_back(Database(database.GetHandle()), operation);
 }
 
-auto Transaction::GetPackageUpdates() const -> std::vector<Package> {
-    auto updates = m_packageOperations | std::views::filter([](std::pair<Package, PackageOperation> pair) -> bool {return pair.second == PackageOperation::Update;}) | std::views::keys;
-
-    return std::ranges::to<std::vector<Package>>(updates);
+auto Transaction::AddSystemUpgradeOperation() -> void {
+    m_systemUpgrade = true;
 }
 
 auto Transaction::GetPackageInstalls() const -> std::vector<Package> {
@@ -40,4 +42,89 @@ auto Transaction::GetDatabaseUpdates() const -> std::vector<Database> {
     auto updates = m_databaseOperations | std::views::filter([](std::pair<Database, DatabaseOperation> pair) -> bool {return pair.second == DatabaseOperation::Update;}) | std::views::keys;
 
     return std::ranges::to<std::vector<Database>>(updates);
+}
+
+auto Transaction::Apply() const -> void {
+    if (alpm_trans_init(ALPM::GetHandle(), static_cast<alpm_transflag_t>(m_transactionFlags.to_ulong() & 0b111111111111111111)) != 0) {
+        throw std::runtime_error(std::format("Failed to apply transaction: Failed to initialize libalpm transaction: {}", ALPM::GetError()));
+    }
+
+    for (const auto &[package, operation] : m_packageOperations) {
+        switch (operation) {
+            case PackageOperation::Install:
+                if (alpm_add_pkg(ALPM::GetHandle(), package.GetHandle()) != 0) {
+                    throw std::runtime_error(std::format("Failed to apply transaction: Couldn't add package to libalpm transaction: {}", ALPM::GetError()));
+                }
+                break;
+            case PackageOperation::Uninstall:
+                if (alpm_remove_pkg(ALPM::GetHandle(), package.GetHandle()) != 0) {
+                    throw std::runtime_error(std::format("Failed to apply transaction: Couldn't add package to libalpm transaction: {}", ALPM::GetError()));
+                }
+                break;
+        }
+    }
+
+    
+    if (!GetDatabaseUpdates().empty()) {
+        alpm_list_t *list = new alpm_list_t;
+        list->prev = nullptr;
+        alpm_list_t *dbList = list;
+        for (const Database &database : GetDatabaseUpdates()) {
+            // Handle first list entry
+            if (dbList == list) {
+                dbList->data = database.GetHandle();
+                continue;
+            }
+
+            // Sets up new entry in list
+            dbList->next = new alpm_list_t;
+            dbList = dbList->next;
+
+            dbList->data = database.GetHandle();
+        }
+
+        // TODO: Allow modifying force flag
+        if (alpm_db_update(ALPM::GetHandle(), list, false) != 0) {
+            alpm_list_free(list);
+            dbList = nullptr;
+
+            throw std::runtime_error(std::format("Failed to apply transaction: Could not add database updates to libalpm transaction: {}", ALPM::GetError()));
+        }
+    }
+
+    if (m_systemUpgrade) {
+        if (alpm_sync_sysupgrade(ALPM::GetHandle(), false) != 0) {
+            throw std::runtime_error(std::format("Failed to apply transaction: Could not add packages to upgrade: {}", ALPM::GetError()));
+        }
+    }
+
+    alpm_list_t *errorList = nullptr;
+    if (alpm_trans_prepare(ALPM::GetHandle(), &errorList) != 0) {
+        if (errorList) {
+            // TODO: Handle conflicting packages
+
+            alpm_list_free(errorList);
+            errorList = nullptr;
+        }
+        throw std::runtime_error(std::format("Failed to prepare transaction: {}", ALPM::GetError()));
+    }
+
+    if (errorList) {
+        // TODO: Handle conflicting packages
+
+        alpm_list_free(errorList);
+        errorList = nullptr;
+    }
+
+    
+    if (alpm_trans_commit(ALPM::GetHandle(), &errorList) != 0) {
+        if (errorList) {
+            // TODO: Handle possible errors
+
+            alpm_list_free(errorList);
+            errorList = nullptr;
+        }
+
+        throw std::runtime_error(std::format("Failed to commit transaction: {}", ALPM::GetError()));
+    }
 }
